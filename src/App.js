fb9f275bf5dfac176c2c143139765be2e2e8a179
Manish Devrani <79859472+manish591@users.noreply.github.com>
2021-07-13 18:35:34 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -3,8 +3,18 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>bmi calculator</h1>
+      <h2>Info</h2>
+      <div className="div">
+        <label>
+          Weight(in Kg)
+          <input type="number" min="1" />
+        </label>
+        <label>
+          Height(in cm)
+          <input type="number" min="1" />
+        </label>
+      </div>
     </div>
   );
 }
