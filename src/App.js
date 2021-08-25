7410eb4dcd8cfa467a61cf2094c406817c28aa2c
Manish Devrani <79859472+manish591@users.noreply.github.com>
2021-08-25 16:29:58 +0530

    added bmi calculation logic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
+import { useState } from "react";
 import "./styles.css";
 
+let inputWeight = 0;
+let inputHeight = 0;
+
 export default function App() {
+  const [output, setOutput] = useState("Your output will show here!");
+
+  function readInputHeight(e) {
+    inputHeight = e.target.value;
+  }
+
+  function readInputWeight(e) {
+    inputWeight = e.target.value;
+  }
+
+  function calculateBmi() {
+    let height = inputHeight / 100;
+    console.log(inputHeight);
+    console.log(height);
+    let calculation = inputWeight / (height * height);
+    setOutput(`Your BMI is ${calculation}`);
+  }
+
   return (
     <div className="App">
       <h1>bmi calculator</h1>
@@ -8,13 +30,15 @@ export default function App() {
       <div className="div">
         <label>
           Weight(in Kg)
-          <input type="number" min="1" />
+          <input onChange={readInputWeight} type="number" min="1" />
         </label>
         <label>
           Height(in cm)
-          <input type="number" min="1" />
+          <input onChange={readInputHeight} type="number" min="1" />
         </label>
+        <button onClick={calculateBmi}>Calculate</button>
       </div>
+      <div>{output}</div>
     </div>
   );
 }
